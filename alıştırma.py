971e5cb9bf9fed5387ff07d5b3539f725fdcf29c
phunser <141871711+phunser@users.noreply.github.com>
2023-10-10 12:49:46 +0300

    Add files via upload

---
+++ alıştırma.py
@@ -0,0 +1,17 @@
+
+menu=("1)adana 2)mersin 3)antep 4)konya")
+print(menu)
+while True:
+    şehir =input("Bir şehir seçiniz? :")
+    if şehir == "1":
+        print("adana ya hoş geldiniz")
+    elif şehir =="2":
+        print("mersine hoş geldiniz")
+    elif şehir=="3":
+        print("antep güzel memleket")
+    elif şehir =="4":
+        print("neden konya")
+    else:
+        print("yanlış karakter!")
+
+            
